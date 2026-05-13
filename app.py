1f3da2b3ad43e5d729b891c9d61b3a1e0ca644b6
shenodarezk92-svg <shenodarezk92@gmail.com>
2026-05-12 22:58:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def make_video():
     image_url = data['image_url']
 
     uid = str(uuid.uuid4())[:8]
-    img_path = f"/tmp/{uid}.jpg"
+    img_path = f"/tmp/{uid}.png"
     out_path = f"/tmp/{uid}.mp4"
 
     img_data = requests.get(image_url, timeout=30)
@@ -27,10 +27,9 @@ def make_video():
         "-i", "music.mp3",
         "-c:v", "libx264",
         "-preset", "ultrafast",
-        "-tune", "stillimage",
         "-c:a", "aac",
         "-t", "15",
-        "-vf", "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2,setsar=1",
+        "-vf", "scale=1080:1920,setsar=1",
         "-pix_fmt", "yuv420p",
         out_path
     ]
